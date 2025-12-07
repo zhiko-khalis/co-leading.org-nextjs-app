3bcda5a8f744b13da4db8e24c6b631f13f83d77c
zhiko-khalls <zhekoxalis11@gmail.com>
2025-12-07 15:13:07 +0300

    [REMOVE] Removing the scroll icon

--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -70,11 +70,11 @@ export default function Hero() {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10 animate-bounce">
+      {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10 animate-bounce">
         <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center pt-2">
           <div className="w-1 h-3 bg-white/50 rounded-full"></div>
         </div>
-      </div>
+      </div> */}
     </section>
     </>
   );
